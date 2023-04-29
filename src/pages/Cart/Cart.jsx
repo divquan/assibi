f2c08e985b2d68f0231ec58e24e803d053e682f5
divquan <97671400+divquan@users.noreply.github.com>
2023-04-29 20:27:53 +0000

    fix: shopping cart"

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -51,7 +51,9 @@ const Cart = () => {
                     <div className="d-flex justify-content-between align-items-center mb-4">
                       <div>
                         <p className="mb-1">Shopping cart</p>
-                        <p className="mb-0">You have 4 items in your cart</p>
+                        <p className="mb-0">
+                          You have {shoppingCarts?.length} items in your cart
+                        </p>
                       </div>
                       <div>
                         <p>
